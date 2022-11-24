039543d22fdb33ddd2d8465efd5d8603b5285da0
cialese <cinthyaless@gmail.com>
2022-11-24 17:51:52 -0500

    add algolia integration

--- .eleventy.cjs
+++ .eleventy.cjs
@@ -24,6 +24,25 @@ module.exports = function(eleventyConfig) {
     return formatDate(date, format);
   });
 
+  //filters
+  eleventyConfig.addFilter("squash", function(text) {
+    const content = text.toString().toLowerCase();
+
+    // remove duplicated words
+    const words = content.split(' ');
+    const deduped = [...(new Set(words))];
+    const dedupedStr = deduped.join(' ')
+
+    // remove short and less meaningful words
+    let result = dedupedStr.replace(/\b(\.|\,|the|a|an|and|am|you|I|to|if|of|off|me|my|on|in|it|is|at|as|we|do|be|has|but|was|so|no|not|or|up|for)\b/gi, '');
+    //remove newlines, and punctuation
+    result = result.replace(/\.|\,|\?|-|—|\n/g, '');
+    //remove repeated spaces
+    result = result.replace(/[ ]{2,}/g, ' ');
+
+    return result;
+  })
+
 
   return {
     templateFormats: [
