fe0d91daf238ded9af55db070ee0d34fe1549b07
cialese <cinthyaless@gmail.com>
2022-12-22 19:07:26 -0500

    add plugins to config

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,5 @@
 import {existsSync, readFileSync} from 'node:fs';
+import {reportError} from "./tools";
 
 type AlgoliaConfig = {
 	algoliaAppId: string;
@@ -13,6 +14,7 @@ export class Config {
 	templatePath = 'template';
 	searchEngine = 'algolia';
 	algolia?: AlgoliaConfig;
+	plugins?: any;
 	imagesPath = 'images';
 	assetsPath = 'css';
 
@@ -28,23 +30,50 @@ export class Config {
 	}
 
 	loadConfig(path: string) {
-		const data = readFileSync(path, {encoding: 'utf8'});
-		const config = JSON.parse(data) as Record<string, string>;
-		this.originPath = config.originPath ?? this.originPath;
-		this.outputPath = config.outputPath ?? this.outputPath;
-		this.dataPath = config.dataPath ?? this.dataPath;
-		this.templatePath = config.templatePath ?? this.templatePath;
-		this.searchEngine = config.searchEngine ?? this.searchEngine;
-		if (config.algoliaAppId && config.algoliaKey && config.algoliaIndexName) {
-			this.algolia = {
-				algoliaAppId: config.algoliaAppId,
-				algoliaKey: config.algoliaKey,
-				algoliaIndexName: config.algoliaIndexName,
-			};
-		}
+		try {
+			const data = readFileSync(path, {encoding: 'utf8'});
+			const JSONconfig = JSON.parse(data) as Record<string, any>;
 
-		this.imagesPath = config.imagesPath ?? this.imagesPath;
-		this.assetsPath = config.assetsPath ?? this.assetsPath;
+			this.originPath = JSONconfig.originPath ?? this.originPath;
+			this.outputPath = JSONconfig.outputPath ?? this.outputPath;
+			this.dataPath = JSONconfig.dataPath ?? this.dataPath;
+			this.templatePath = JSONconfig.templatePath ?? this.templatePath;
+			this.searchEngine = JSONconfig.searchEngine ?? this.searchEngine;
+			if (JSONconfig.algoliaAppId && JSONconfig.algoliaKey && JSONconfig.algoliaIndexName) {
+				this.algolia = {
+					algoliaAppId: JSONconfig.algoliaAppId,
+					algoliaKey: JSONconfig.algoliaKey,
+					algoliaIndexName: JSONconfig.algoliaIndexName,
+				};
+			}
+
+			this.imagesPath = JSONconfig.imagesPath ?? this.imagesPath;
+			this.assetsPath = JSONconfig.assetsPath ?? this.assetsPath;
+
+			if(JSONconfig.plugins && Array.isArray(JSONconfig.plugins)) {
+				if(JSONconfig.plugins.length)  {
+					const validPlugins = JSONconfig.plugins.every((plugin) => typeof plugin === 'string');
+					if(validPlugins) {
+						JSONconfig.plugins.forEach((name: string) => {
+							this.loadPlugins(name, JSONconfig[name]);
+						})
+					} else {
+						throw 'Invalid plugins';
+					}
+				}
+			} else {
+				throw 'Plugins must be an array of strings'
+			}
+		} catch(error: unknown) {
+			reportError(error);
+		}
+	}
+
+
+	loadPlugins(name: string, config: Record<string, string>) {
+		if(config) {
+			this.plugins[name] = config;
+		}
 	}
 
 	checkConfigFile(path?: string): boolean {
