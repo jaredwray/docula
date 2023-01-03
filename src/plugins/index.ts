7f70e799458709de551188cb8bd7a326ee9ee11d
cialese <cinthyaless@gmail.com>
2023-01-03 13:32:09 -0500

    add robots plugin

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -1,13 +1,15 @@
 import {type GithubConfig, GithubPlugin} from './github.js';
 import {type NpmConfig, NpmPlugin} from './npm.js';
+import {RobotsConfig, RobotsPlugin} from "./robots.js";
 
 const plugins = {
 	github: GithubPlugin,
 	npm: NpmPlugin,
+	robots: RobotsPlugin
 };
 
-export type PluginConfig = GithubConfig | NpmConfig;
+export type PluginConfig = GithubConfig | NpmConfig | RobotsConfig;
 
-export type PluginInstance = GithubPlugin | NpmPlugin;
+export type PluginInstance = GithubPlugin | NpmPlugin | RobotsConfig;
 
 export default plugins;
