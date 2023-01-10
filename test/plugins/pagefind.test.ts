40fdfbe0a7aee9489426bc5b61c904df13895a3d
cialese <cinthyaless@gmail.com>
2023-01-10 11:55:12 -0500

    update pagefind init test

--- test/plugins/pagefind.test.ts
+++ test/plugins/pagefind.test.ts
@@ -1,4 +1,5 @@
 import fs from 'fs-extra';
+import child_process from 'node:child_process';
 import {Config} from '../../src/config.js';
 import {PagefindPlugin} from '../../src/plugins/pagefind.js';
 
@@ -13,13 +14,7 @@ describe('Pagefind Plugin', () => {
 	});
 
 	it('init', () => {
-		const jsonConfig = {
-			...defaultConfig,
-			pagefind: {
-				output: 'test/dist',
-			},
-		};
-		fs.writeFileSync('test/data/pagefind-config.json', JSON.stringify(jsonConfig, null, 2));
+		fs.writeFileSync('test/data/pagefind-config.json', JSON.stringify(defaultConfig, null, 2));
 		const config = new Config('./test/data/pagefind-config.json');
 		const pagefind = new PagefindPlugin(config);
 		expect(pagefind).toBeDefined();
