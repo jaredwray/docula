3bd4d5dc7566d8a7701de4e3c712e47e25210024
Christian Llontop <christian@rhinolabs.co>
2023-02-17 19:07:30 -0500

    format code refactor

--- src/eleventy/shortcodes.ts
+++ src/eleventy/shortcodes.ts
@@ -3,9 +3,9 @@ import MarkdownIt from 'markdown-it';
 
 export const getYear = (): string => DateTime.local().toUTC().toFormat('yyyy');
 
-export const formatDate = (format: string, date?: Date): string => {
+export const formatDate = (format: string, date?: string): string => {
 	if (date) {
-		return DateTime.fromJSDate(date).toUTC().toFormat(format);
+		return DateTime.fromISO(date).toUTC().toFormat(format);
 	}
 
 	return DateTime.now().toUTC().toFormat(format);
