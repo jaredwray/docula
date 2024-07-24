16bc48327ea326775cc1c86d05241f5ec4470e7b
Jared Wray <me@jaredwray.com>
2024-07-24 11:39:26 -0700

    version bump to v0.7.0

--- src/builder.ts
+++ src/builder.ts
@@ -257,6 +257,15 @@ export class DoculaBuilder {
 		const sitemapPath = `${data.outputPath}/sitemap.xml`;
 		const urls = [{url: data.siteUrl}, {url: `${data.siteUrl}/releases`}];
 
+		// Add all the document urls
+		for (const document of data.documents ?? []) {
+			let urlPath = document.urlPath;
+			if (urlPath.endsWith('index.html')) {
+				urlPath = urlPath.slice(0, -10);
+			}
+			urls.push({url: `${data.siteUrl}${urlPath}`});
+		}
+
 		let xml = '<?xml version="1.0" encoding="UTF-8"?>';
 		xml += '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
 
@@ -493,7 +502,6 @@ export class DoculaBuilder {
 		if(documentPath.slice(documentsFolderIndex+6).indexOf('/') === -1) {
 			isRoot = true;
 			const filePath = documentPath.slice(documentsFolderIndex+6);
-			console.log('filePath', filePath);
 			if(filePath == 'index.md') {
 				urlPath = documentPath.slice(documentsFolderIndex).replace('.md', '.html');
 			}
