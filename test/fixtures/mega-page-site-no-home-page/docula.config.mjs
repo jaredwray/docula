5fd5eaffbb3de5dbcf70d72791b3c5336dd39238
Jared Wray <me@jaredwray.com>
2026-03-02 22:14:18 -0800

    fixture system

---
+++ test/fixtures/mega-page-site-no-home-page/docula.config.mjs
@@ -0,0 +1,13 @@
+export const options = {
+	outputPath: "./test/fixtures/mega-page-site-no-home-page/dist",
+	sitePath: "./test/fixtures/mega-page-site-no-home-page",
+	githubPath: "jaredwray/docula",
+	siteTitle: "docula",
+	siteDescription: "Beautiful Website for Your Projects",
+	siteUrl: "https://docula.org",
+	sections: [
+		{ name: "Caching", path: "caching", order: 5 },
+		{ name: "Compression", path: "compression", order: 4 },
+		{ name: "Storage Adapters", path: "storage-adapters", order: 3 },
+	],
+};
