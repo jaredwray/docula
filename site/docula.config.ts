68deecffe296eb36f7ff527bc4bcb769fa14930c
Jared Wray <me@jaredwray.com>
2026-03-22 16:09:21 -0700

    editpageurl

--- site/docula.config.ts
+++ site/docula.config.ts
@@ -11,6 +11,7 @@ export const options: Partial<DoculaOptions> = {
 	siteDescription: 'Beautiful Website for Your Projects',
 	siteUrl: 'https://docula.org',
 	themeMode: 'light',
+	editPageUrl: "https://github.com/jaredwray/docula/blob/main/site/docs",
 	sections: [
 		{name: 'Project Guidelines', path: 'project-guidelines', order: 21},
 	],
